ec315155fbeae8612bf01fb75b23b909b957937a
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-11-10 16:20:55 -0300

    adiciona lógica de chat

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,42 @@
 const express = require('express');
-
 const app = express();
+const PORT = 3000;
 
-app.get('/', (_request, response) => {
-  response.send();
+const http = require('http').Server(app);
+const cors = require('cors');
+
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:5173', // url aceita pelo cors
+        methods: ['GET', 'POST'], // Métodos aceitos pela url
+      },
 });
 
-app.use(express.json());
-app.use(express.static('public'))
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
 
-module.exports = app;
+    socket.on('message', (data) => {
+        console.log(data);
+        socketIO.emit('messageResponse', data);
+      });
+    
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
